075f0990daedcce1b2bf323ded1d7dcfc67be126
Edvaldo Correia <edvaldo.correia@gmail.com>
2020-03-29 21:11:48 -0300

    commit mobile

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,26 +4,24 @@ module.exports = {
   async index(request, response){
     const { page = 1 } = request.query;
 
-    const [count] = await connection('incidents').count('*');
-
-    console.log(count);
-
-    response.header('X-Total-Count', count['count(*)']);
-
-    const incident = await connection('incidents')
-      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+    const [count] = await connection('incidents').count();
+    
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
-      .offset(( page - 1) * 5 )
+      .offset((page - 1) * 5)
       .select([
         'incidents.*', 
-       'ongs.name', 
-       'ongs.email',
-       'ongs.whatsapp',
-       'ongs.city', 
-       'ongs.uf'
+        'ongs.name', 
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city', 
+        'ongs.uf'
       ]);
-    
-    return response.json(incident);
+      
+    response.header('X-Total-Count', count['count(*)']);
+
+    return response.json(incidents);
   },
   
   async create(request, response){
